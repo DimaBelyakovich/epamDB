d84d3b88ba6e96bb8d5bd762c7ba6c2cd0401f09
DimaBelyakovich <dimabelyakovich.mmf@gmail.com>
2020-03-30 14:51:37 +0300

    fix parwntheses bugs

--- MMF_9_2/install/create_tablespaces.sql
+++ MMF_9_2/install/create_tablespaces.sql
@@ -2,13 +2,13 @@ DEFINE tbs_name = &&1
 DEFINE tbs_location = &&2
 DEFINE tbs_type =  &&3
 
-DEFINE l_tbs = &tbs_name'_'&tbs_type
-DEFINE tbsFile = &l_tbs'.dbf'
+DEFINE l_tbs = &tbs_name_&tbs_type
+DEFINE tbsFile = &l_tbs.dbf
 
-CREATE TABLESPACE &&l_tbs{
+CREATE TABLESPACE &&l_tbs
     DATAFILE &&tbsFile
     SIZE 10m AUTOEXTEND ON NEXT 10M MAXSIZE 10G EXTENT MANAGEMENT LOCAL SEGMENT SPACE MANAGEMENT AUTO;
-}
+
 
 UNDEFINE tbs_name
 UNDEFINE tbs_location
