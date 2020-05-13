7c64f0045987684510550006ebda99b2282094d9
DimaBelyakovich <dimabelyakovich.mmf@gmail.com>
2020-05-13 14:26:35 +0300

    upgrade003 main.sql

--- upgrade/main.sql
+++ upgrade/main.sql
@@ -4,18 +4,16 @@ DEFINE USER_NAME = &&1
 
 CONNECT &&USER_NAME/oracle
 
-@./upgrade002/create_table_driver.sql
-@./upgrade002/create_table_event_result_table.sql
-@./upgrade002/create_table_event_track_table.sql
-@./upgrade002/create_table_event.sql
-@./upgrade002/create_table_season_event_table.sql
-@./upgrade002/create_table_season_track_table.sql
-@./upgrade002/create_table_season.sql
-@./upgrade002/create_table_team_driver_table.sql
-@./upgrade002/create_table_team_season_table.sql
-@./upgrade002/create_table_team.sql
-@./upgrade002/create_table_track.sql
-@./upgrade002/create_fk.sql
+@./upgrade003/create_table_driver_statistic.sql
+@./upgrade003/create_table_driver.sql
+@./upgrade003/create_table_event_result.sql
+@./upgrade003/create_table_event.sql
+@./upgrade003/create_table_season_event.sql
+@./upgrade003/create_table_season.sql
+@./upgrade003/create_table_team_statistic.sql
+@./upgrade003/create_table_team.sql
+@./upgrade003/create_table_track.sql
+@./upgrade003/create_fk.sql
 
 UNDEFINE USER_NAME
 SPOOL OFF
